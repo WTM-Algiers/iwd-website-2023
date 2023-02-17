43c25e88313b40405c0b879b5fc53b64dadc62e0
khemissimaroua <khemissimaroua@yahoo.com>
2023-02-17 13:04:26 +0100

    added mentorsJudges section

--- src/components/Mentors&Judges/index.jsx
+++ src/components/Mentors&Judges/index.jsx
@@ -1,7 +1,21 @@
+import CardMentor from './card'
 const MentorsJudges = () => {
     return (
-        <section>
-            {/* Let's go */}
+        <section className=" h-screen">
+            <center className="visible text-3xl font-bold lg:hidden pb-8">Mentors & Judges</center>
+            <div className="absolute"><CardMentor/></div>
+            <div className="flex flex-row">
+            <div className="bg-[#4EA2F7] w-1/4 h-screen"></div>
+                
+                <div className="w-1/2">
+                <center className="invisible lg:visible lg:text-5xl 2xl:text-7xl font-bold pt-8">Mentors & Judges</center>
+                </div>
+                <div className="bg-gradient-to-r from-[#4EA2F7] to-[#5AD1CF] w-1/4 h-screen"></div>
+            </div>
+            <div>
+            
+            </div>
+            
         </section>
     )
 }

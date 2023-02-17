57fceb9aec79ccbc624f6f81b5b2bb0eebc38dd9
Razane Meguellati <kl_meguellati@esi.dz>
2023-02-18 00:01:41 +0100

    responsive version

--- src/components/Workshops/index.jsx
+++ src/components/Workshops/index.jsx
@@ -19,19 +19,19 @@ const Workshops = () => {
 
     return (
         <section>
-            <div className="h-screen w-full flex flex-col items-center justify-start ">
+            <div className="h-screen w-full flex flex-col items-center justify-center lg:justify-start mb-20 ">
 
                     <div>
-                         <h1 className=" font-PTsans font-bold text-7xl "> Workshops</h1>
+                         <h1 className=" font-PTsans font-bold text-3xl  pb-40 lg:pb-0 lg:text-7xl 2xl:text-8xl"> Workshops</h1>
                     </div>
 
-                    <div className="flex justify-center items-center w-full py-16 lg:p-24 relative " >
-                            <div className="absolute bg-iwd-dark lg:h-96 lg:w-24 left-0 top-40 rounded-tr-full rounded-br-full"></div>
-                            <div className="absolute bg-iwd-dark lg:h-96 lg:w-24 right-0 top-1 rounded-tl-full rounded-bl-[50px]"></div>
+                    <div className="flex justify-center items-center w-full py-12 lg:py-16 lg:p-24 relative " >
+                            <div className="absolute invisible lg:visible bg-iwd-dark lg:h-96 lg:w-24 left-0 lg:top-40 2xl:top-96 rounded-tr-full rounded-br-full"></div>
+                            <div className="absolute invisible lg:visible bg-iwd-dark lg:h-96 lg:w-24 right-0 lg:top-1 2xl:-top-10 rounded-tl-full rounded-bl-[50px]"></div>
 
 
                            
-                                <div className="flex items-center justify-center absolute top-20">
+                                <div className="flex items-center justify-center absolute  lg:top-20 2xl:top-32">
                                     <div
                                         onClick={() => {
                                             swiperRef.current.swiper.slidePrev();
@@ -45,7 +45,7 @@ const Workshops = () => {
                                     ref={swiperRef}
                                     modules={[Pagination, Navigation]}
                                     pagination={true}
-                                    className=" flex justify-center items-center w-96 lg:w-[450px] "
+                                    className=" flex justify-center items-center w-[500px] lg:w-[450px] 2xl:w-[700px] "
                                     >
                                     {workshopsData.map((wrkshop, idx) => {
                                         return (
@@ -58,9 +58,9 @@ const Workshops = () => {
                                                         </div>
 
                                                     <div >
-                                                       <div className=" mb-16 relative mx-auto flex px-12 py-3 lg:py-5 lg:px-16  rounded-3xl justify-center text-[12px] lg:text-20 font-semibold">
-                                                           <img src={titlecover} className="h-16 w-80 absolute z-10 top-3" alt="cover" /> 
-                                                           <p className="text-center text-xl p-2"> {wrkshop.description} </p>
+                                                       <div className=" mb-16 relative mx-auto flex px-12 py-3 lg:py-5 lg:px-16  rounded-3xl justify-center ">
+                                                           <img src={titlecover} className=" absolute h-10 lg:h-16 lg:w-80 2xl:h-20 2xl:w-[800px] z-10 top-0 lg:top-3" alt="cover" /> 
+                                                           <p className="text-center font-semibold text-xs lg:text-xl 2xl:text-3xl lg:p-2"> {wrkshop.description} </p>
                                                         </div>
                                                     </div>
                                                        

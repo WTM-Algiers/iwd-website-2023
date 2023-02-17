1f1d67e4287cfe9b79c05723727f31994d9331de
Brivan-26 <ma.rezazi@esi-sba.dz>
2023-02-17 10:05:52 +0100

    cleaned tailwind classes, adjusted HTML tags

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,213 +1,85 @@
 const About = () => {
-    return (
-        <section class="about-cntnr w-full w-screen flex flex-col sm:overflow-hidden justify-center items-center relative">
+  return (
+    <section className="about-cntnr w-screen flex flex-col sm:overflow-hidden justify-center items-center relative">
+      {/* Divs Horizontally */}
+      <img
+        className="upper-border hidden sm:block absolute  sm:left-1/2 sm:top-10 sxm:top-2 lxm:top-10  2xl:hidden"
+        src="\src\assets\About\upper-border-md.png"
+        alt="upper border"
+      />
+      <img
+        className="upper-border hidden 2xl:block absolute 2xl:left-1/2 2xl:top-10  "
+        src="\src\assets\About\upper-border-lg.png"
+        alt="down border"
+      />
 
-            {/* with grid  */}
-            
-            {/* <div class="grid grid-cols-2 justify-items-center place-items-center gap-7 h-screen pt-7 pb-7">
-            
-                <div>
-                    <img src="\src\assets\About\about-upper-img.png" alt="Boo!" />
-                </div>
+      <div className="flex flex-col items-center sm:w-full sm:flex-row">
+        <div className="w-full px-[20px] sm:w-1/2 sm:order-3 sm:p-10 sm:pt-0 lg:pl-20">
+          <h1 className="text-center p-3 sm:pt-8 sm:mt-10 md:mt-0 font-bold sm:order-1 text-[20px] md:text-[27px] 2xl:text-[40px]">
+            What is WTM Algiers ?
+          </h1>
+          <p className="text-justify p-3 paragraph-sizing">
+            Women Techmakers is a program created by Google, that encourages
+            women to integrate, pursue, and outshine in technology careers
+            through their Google Developers Groups communities dispersed
+            worldwide. Women Techmakers Algiers (WTM Algiers) is part of this
+            international community that helps empower and improve the
+            enrollment of women in the STEM field.
+          </p>
+        </div>
+        <img
+          className="p-3 w-2/3 sm:w-1/2 sm:p-10 sm:items-end  sm:pb-0 lg:pr-20"
+          src="\src\assets\About\about-upper-img.png"
+          alt="upper border"
+        />
+      </div>
 
-                <div class="flex items-center flex-col p-7">
-                    <h1 class="about-title">
-                        What is WTM Algiers ?
-                    </h1>
-                    <p class="">
-                        Women Techmakers is a program created by Google, that encourages women to integrate, pursue, and outshine in technology careers through their Google Developers Groups communities dispersed worldwide. 
-                        Women Techmakers Algiers (WTM Algiers) is part of this international community that helps empower and improve the enrollment of women in the STEM field.
+      <img
+        className="about-element hidden sm:block sxm:hidden lxm:block absolute sm:insert-x-1/2 2xl:hidden"
+        src="\src\assets\About\about-element-md.png"
+        alt="shape"
+      />
+      <img
+        className="about-element hidden 2xl:block pl-12 absolute 2xl:insert-x-1/2"
+        src="\src\assets\About\about-element-lg.png"
+        alt="shape"
+      />
 
-                    </p>
-                </div>
+      <div className="flex flex-col items-center sm:w-full sm:flex-row">
+        <div className="w-full px-[20px] sm:w-1/2 sm:p-10 sm:pt-0 lg:pr-20">
+          <h1 className="text-center p-3 sm:pt-8 font-bold text-[20px] md:text-[27px] 2xl:text-[40px]">
+            What is IWD?
+          </h1>
+          <p className="text-justify p-3 pb-0 paragraph-sizing">
+            Iwd stands for international women's day, an annual event organized
+            by all Women Techmakers chapters in the world to highlight the
+            contributions of women in tech fields to educate and inspire our
+            community.
+          </p>
+          <p className="text-justify p-3 pt-0 paragraph-sizing">
+            Last year's theme was ‘progress not perfection’ but this year's is
+            ‘DareToBe’ to celebrate the woman who challenges their fear to
+            improve and demonstrate their existence in the STEM world.
+          </p>
+        </div>
+        <img
+          className="p-3 w-2/3 sm:w-1/2 sm:-mt-20 md:-mt-30 sm:items-start sm:p-10 sm:pt-0 lg:pl-20 "
+          src="\src\assets\About\about-lower-img-md.png"
+          alt="shape"
+        />
+      </div>
+      <img
+        className="hidden sm:block absolute sm:right-1/2 sm:-mr-5 sm:bottom-10 sxm:bottom-2 lxm:bottom-10 2xl:hidden"
+        src="\src\assets\About\lower-border-md.png"
+        alt="shape"
+      />
+      <img
+        className="hidden 2xl:block absolute 2xl:right-1/2 2xl:-mr-5 2xl:bottom-10"
+        src="\src\assets\About\lower-border-lg.png"
+        alt="shape"
+      />
+    </section>
+  );
+};
 
-                <div class="flex items-center flex-col p-7">
-                    <h1 class="about-title">
-                        What is IWD?
-                    </h1>
-                    <p class="">
-                        Iwd stands for international women's day, an annual event organized by all Women Techmakers chapters in the world to highlight the contributions of women in tech fields to educate and inspire our community.
-                    </p>                    
-                    <p>
-                        Last year's theme was ‘progress not perfection’ but this year's is ‘DareToBe’ to celebrate the woman who challenges their fear to improve and demonstrate their existence in the STEM world.
-                    </p>
-                </div>
-
-                <div>
-                    <img src="\src\assets\About\about-lower-img.png" alt="Boo!" />
-                </div>
-
-            </div>  */}
-
-            {/* Dins Vertically  */}
-
-            {/* <img class="upper-border visible sm:hidden" src="\src\assets\About\upper-border.png" alt="..." />
-            <div class=" flex items-center  h-screen">
-
-                <div class="w-1/2 h-screen p-7 pb-15">
-                    <div class="flex items-center justify-center p-4" >
-                        <img src="\src\assets\About\about-upper-img.png" alt="Boo!" />
-                    </div>
-                    <div>
-                        <h1 class="text-center p-4 font-bold">
-                            What is IWD?
-                        </h1>
-                        <p class="text-justify p-4 pb-0">
-                            Iwd stands for international women's day, an annual event organized by all Women Techmakers chapters in the world to highlight the contributions of women in tech fields to educate and inspire our community.
-                        </p>                    
-                        <p class="text-justify p-4 pt-0">
-                            Last year's theme was ‘progress not perfection’ but this year's is ‘DareToBe’ to celebrate the woman who challenges their fear to improve and demonstrate their existence in the STEM world.
-                        </p>
-                    </div>
-                </div>
-                <img class="about-element visible sm:hidden" src="\src\assets\About\about-element.png" alt="..." />
-                
-                <div class="w-1/2 h-screen p-7 pt-15">
-                    <div class="h-1/6">
-                        
-                    </div>
-                    <div>
-                        <h1 class="text-center p-4 font-bold">
-                            What is WTM Algiers ?
-                        </h1>
-                        <p class="text-justify p-4">
-                            Women Techmakers is a program created by Google, that encourages women to integrate, pursue, and outshine in technology careers through their Google Developers Groups communities dispersed worldwide. 
-                            Women Techmakers Algiers (WTM Algiers) is part of this international community that helps empower and improve the enrollment of women in the STEM field.
-                        </p>
-                    </div>
-                    <div class="flex items-center justify-center p-4" >
-                        <img src="\src\assets\About\about-lower-img.png" alt="Boo!" />
-                    </div>
-                    
-                </div>
-            </div>
-            <img class="lower-border visible sm:hidden" src="\src\assets\About\lower-border.png" alt="..." /> */}
-
-
-            {/* taslik  */}
-
-            {/* <img class="upper-border hidden sm:block" src="\src\assets\About\upper-border.png" alt="..." />
-            <div class=" hidden sm:flex flex-row items-center  h-screen">
-
-                <div class="w-1/2 h-screen p-7 pb-15">
-                    <div class="flex items-center justify-center p-4" >
-                        <img src="\src\assets\About\about-upper-img.png" alt="Boo!" />
-                    </div>
-                    <div class="hidden sm:block">
-                        <h1 class="text-center p-4 font-bold">
-                            What is IWD?
-                        </h1>
-                        <p class="text-justify p-4 pb-0">
-                            Iwd stands for international women's day, an annual event organized by all Women Techmakers chapters in the world to highlight the contributions of women in tech fields to educate and inspire our community.
-                        </p>                    
-                        <p class="text-justify p-4 pt-0">
-                            Last year's theme was ‘progress not perfection’ but this year's is ‘DareToBe’ to celebrate the woman who challenges their fear to improve and demonstrate their existence in the STEM world.
-                        </p>
-                    </div>
-                </div>
-                
-                <img class="about-element hidden sm:block" src="\src\assets\About\about-element.png" alt="..." />
-                
-                <div class="w-1/2 h-screen flex flex-center justify center p-7 pt-15">
-                    <div class="w-fit">
-                        <div class="h-1/6 hidden sm:block">
-                            
-                        </div>
-                        <div class="hidden sm:block">
-                            <h1 class="text-center p-4 font-bold">
-                                What is WTM Algiers ?
-                            </h1>
-                            <p class="text-justify p-4">
-                                Women Techmakers is a program created by Google, that encourages women to integrate, pursue, and outshine in technology careers through their Google Developers Groups communities dispersed worldwide. 
-                                Women Techmakers Algiers (WTM Algiers) is part of this international community that helps empower and improve the enrollment of women in the STEM field.
-                            </p>
-                        </div>
-                        <div class="flex items-center justify-center p-4" >
-                            <img src="\src\assets\About\about-lower-img.png" alt="Boo!" />
-                        </div>
-                    </div>
-                    
-                    
-                </div>
-            </div>
-            <img class="lower-border hidden sm:block" src="\src\assets\About\lower-border.png" alt="..." />
-            <div class="  flex flex-col justify-center  sm:hidden">
-                <div class="">
-                    <h1 class="text-center p-4 font-bold">
-                    What is WTM Algiers ?
-                    </h1>
-                    <p class="text-justify p-4">
-                        Women Techmakers is a program created by Google, that encourages women to integrate, pursue, and outshine in technology careers through their Google Developers Groups communities dispersed worldwide. 
-                        Women Techmakers Algiers (WTM Algiers) is part of this international community that helps empower and improve the enrollment of women in the STEM field.
-                    </p>
-                    <div class="flex items-center justify-center p-4" >
-                        <img src="\src\assets\About\about-upper-img.png" alt="Boo!" />
-                    </div>
-                </div>
-                <div>
-                    <h1 class="text-center p-4 font-bold">
-                        What is IWD?
-                    </h1>
-                    <p class="text-justify p-4 pb-0">
-                        Iwd stands for international women's day, an annual event organized by all Women Techmakers chapters in the world to highlight the contributions of women in tech fields to educate and inspire our community.
-                    </p>                    
-                    <p class="text-justify p-4 pt-0">
-                        Last year's theme was ‘progress not perfection’ but this year's is ‘DareToBe’ to celebrate the woman who challenges their fear to improve and demonstrate their existence in the STEM world.
-                    </p>
-                    <div class="flex items-center justify-center p-4" >
-                        <img src="\src\assets\About\about-lower-img.png" alt="Boo!" />
-                    </div>
-                </div>
-            </div> */}
-
-            {/* Divs Horizontally */}
-
-             <img class="upper-border hidden sm:block absolute  sm:left-1/2 sm:top-10 sxm:top-2 lxm:top-10  2xl:hidden" src="\src\assets\About\upper-border-md.png" alt="..." /> 
-            <img class="upper-border hidden 2xl:block absolute 2xl:left-1/2 2xl:top-10  " src="\src\assets\About\upper-border-lg.png" alt="..." />
-
-            <div class="about-wtm flex flex-col items-center sm:w-full sm:flex-row">
-                
-                <div class="w-2/3 sm:w-1/2 sm:order-3 sm:p-10 sm:pt-0 lg:pl-20">
-
-                
-                    <h1 class="text-center p-3 sm:pt-8 sm:mt-10 md:mt-0 font-bold sm:order-1 text-[20px] md:text-[27px] 2xl:text-[40px]">
-                        What is WTM Algiers ?
-                    </h1>
-                    <p class="text-justify p-3 text-[17px] text-[19px] text-[28px]">
-                        Women Techmakers is a program created by Google, that encourages women to integrate, pursue, and outshine in technology careers through their Google Developers Groups communities dispersed worldwide.
-                        Women Techmakers Algiers (WTM Algiers) is part of this international community that helps empower and improve the enrollment of women in the STEM field.
-                    </p>
-                </div>
-                {/* <div class="hidden sm:block sm:order-2 sm:w-10"></div> */}
-                <img class="p-3 w-2/3 sm:w-1/2 sm:p-10 sm:items-end  sm:pb-0 lg:pr-20" src="\src\assets\About\about-upper-img.png" alt="Boo!" />
-                
-            </div>
-
-            <img class="about-element hidden sm:block sxm:hidden lxm:block absolute sm:insert-x-1/2 2xl:hidden" src="\src\assets\About\about-element-md.png" alt="..." />
-            <img class="about-element hidden 2xl:block absolute  2xl:insert-x-1/2" src="\src\assets\About\about-element-lg.png" alt="..." />
-
-            <div class="about-iwd flex flex-col items-center sm:w-full  sm:flex-row">
-
-                <div class=" w-2/3 sm:w-1/2 sm:p-10 sm:pt-0 lg:pr-20">
-
-                
-                    <h1 class="text-center p-3 sm:pt-8 font-bold text-[20px] md:text-[27px] 2xl:text-[40px]">
-                        What is IWD?
-                    </h1>
-                    <p class="text-justify p-3 pb-0 text-[17px] text-[19px] text-[28px]">
-                        Iwd stands for international women's day, an annual event organized by all Women Techmakers chapters in the world to highlight the contributions of women in tech fields to educate and inspire our community.
-                    </p>                    
-                    <p class="text-justify p-3 pt-0 text-[17px] text-[19px] text-[28px]">
-                        Last year's theme was ‘progress not perfection’ but this year's is ‘DareToBe’ to celebrate the woman who challenges their fear to improve and demonstrate their existence in the STEM world.
-                    </p>
-                </div>
-                {/* <div class="hidden sm:block sm:w-10"></div> */}
-                <img class="p-3 w-2/3 sm:w-1/2 sm:-mt-20 md:-mt-30 sm:items-start sm:p-10 sm:pt-0 lg:pl-20 " src="\src\assets\About\about-lower-img-md.png" alt="Boo!" />
-            </div>
-            <img class="lower-border hidden sm:block absolute sm:right-1/2 sm:-mr-5 sm:bottom-10 sxm:bottom-2 lxm:bottom-10 2xl:hidden" src="\src\assets\About\lower-border-md.png" alt="..." />
-            <img class="lower-border hidden 2xl:block absolute 2xl:right-1/2 2xl:-mr-5 2xl:bottom-10" src="\src\assets\About\lower-border-lg.png" alt="..." />
-        </section>
-    )
-}
-
-export default About
+export default About;

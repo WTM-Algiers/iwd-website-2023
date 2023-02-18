585c54844adeb4a35e74b3d77fc93121e2b57930
Brivan-26 <ma.rezazi@esi-sba.dz>
2023-02-18 18:42:05 +0100

    Adjusted HTML tags

--- src/components/Mentors&Judges/index.jsx
+++ src/components/Mentors&Judges/index.jsx
@@ -1,23 +1,25 @@
-import CardMentor from './card'
+import CardMentor from "./card";
 const MentorsJudges = () => {
-    return (
-        <section className=" h-screen">
-            <center className="block text-3xl font-bold lg:hidden pb-8">Mentors & Judges</center>
-            <div className="absolute"><CardMentor/></div>
-            <div className="flex flex-row">
-            <div className="bg-[#4EA2F7] w-1/4 h-screen"></div>
-                
-                <div className="w-1/2">
-                <center className="invisible lg:visible lg:text-5xl 2xl:text-7xl font-bold pt-8">Mentors & Judges</center>
-                </div>
-                <div className="bg-gradient-to-r from-[#4EA2F7] to-[#5AD1CF] w-1/4 h-screen"></div>
-            </div>
-            <div>
-            
-            </div>
-            
-        </section>
-    )
-}
+  return (
+    <section className="h-screen">
+      <center className="block text-3xl font-bold lg:hidden pb-8">
+        Mentors & Judges
+      </center>
+      <div className="absolute">
+        <CardMentor />
+      </div>
+      <div className="flex flex-row">
+        <div className="bg-[#4EA2F7] w-1/4 h-screen"></div>
 
-export default MentorsJudges
+        <div className="w-1/2">
+          <center className="invisible lg:visible lg:text-5xl 2xl:text-7xl font-bold pt-8">
+            Mentors & Judges
+          </center>
+        </div>
+        <div className="bg-gradient-to-r from-[#4EA2F7] to-[#5AD1CF] w-1/4 h-screen"></div>
+      </div>
+    </section>
+  );
+};
+
+export default MentorsJudges;

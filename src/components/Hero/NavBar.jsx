7d8602933d5fe423bb3a5a61041104b56bbf14d4
Hind26 <ledra.hind@gmail.com>
2023-02-21 17:52:12 +0100

    navbar fixing+ adding elements in the hero section

--- src/components/Hero/NavBar.jsx
+++ src/components/Hero/NavBar.jsx
@@ -1,5 +1,3 @@
-
-import {FaTimes, FaBars } from  "react-icons/fa"
 import { useState } from "react";
 
 import wtmLogo from "../../assets/logos/wtm-algiers.png"; 
@@ -19,54 +17,58 @@ function Navbar(){ //set links here
       const [toggle, setToggle] = useState(false);
     
       return (
-        <nav className="w-full flex py-6 justify-between items-center navbar">
-          <img src={wtmLogo} alt="WTM Logo" className="w-[165px] h-[32px] ml-10" />
-    
-          <ul className="list-none sm:flex hidden justify-end items-center flex-1 mr-10">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`cursor-pointer text-[16px] ${
-                  active === nav.title ? "text-white" : "text-dimWhite"
-                } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-                onClick={() => setActive(nav.title)}
-              >
-                <a href={`#${nav.id}`}>{nav.title}</a>
-              </li>
-            ))}
-            <button className="btn btn-black bg-color-black ml-10"> Register</button>
-          </ul>
-    
-          <div className="sm:hidden flex flex-1 justify-end items-center">
-           
-    <FaBars className="w-[16px] h-[24px] object-contain mr-10"
-              onClick={() => setToggle(!toggle)}></FaBars>
-            
-            <div
-              className={`${
-                !toggle ? "hidden" : "flex"
-              } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-            >
+        <div className="bg-iwd-white w-screen z-20 fixed">
+  
+        <div className="container flex py-6 justify-between align-center items-center">
+         <img src={wtmLogo} alt="WTM Logo" className="w-[9rem]" />
+   
+   <ul className="list-none sm:flex hidden justify-center gap-x-8 items-center">
+     {navLinks.map((nav, index) => (
+       <li
+         key={nav.id}
+         className={`cursor-pointer text-[16px] ${
+           active === nav.title ? "text-white" : "text-dimWhite"
+         }`}
+         onClick={() => setActive(nav.title)}
+       >
+         <a href={`#${nav.id}`}>{nav.title}</a>
+         
+       </li>
+     ))}
+     <button className="bg-iwd-dark px-4 py-1 text-iwd-white rounded-full">Register</button>
+   </ul>
 
-            <nav>
-              <ul className="list-none flex justify-end items-start flex-1 flex-col">
-                {navLinks.map((nav, index) => (
-                  <li
-                    key={nav.id}
-                    className={`cursor-pointer text-[16px] ${
-                      active === nav.title ? "text-white" : "text-dimWhite"
-                    } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-                    onClick={() => setActive(nav.title)}
-                  >
-                    <a href={`#${nav.id}`}>{nav.title}</a>
-                  </li>
-                ))}
-              </ul>
-              </nav>
-            </div>
-          </div>
-        </nav>
-      );
-    };
+   <div className="sm:hidden flex flex-1 justify-end items-center">
+    
+<div className="flex flex-col gap-2">
+<div className="h-[2px] w-9 bg-iwd-black"></div>  <div className="h-[2px] w-9 bg-iwd-black"></div>  
+<div className="h-[2px] w-9 bg-iwd-black"></div>
+   </div>
+     
+     <div
+       className={`${
+         !toggle ? "hidden" : "flex"
+       } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+     >
+
+
+       <ul className="list-none flex justify-end items-start flex-1 flex-col">
+         {navLinks.map((nav, index) => (
+           <li
+             key={nav.id}
+             className={`cursor-pointer text-[16px] ${
+               active === nav.title ? "text-white" : "text-dimWhite"
+             } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+             onClick={() => setActive(nav.title)}
+           >
+             <a href={`#${nav.id}`}>{nav.title}</a>
+           </li>
+         ))}
+       </ul>
+       </div>
+     </div>
+   </div>
+   </div>
+     )};
 
 export default Navbar;
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,5 +1,56 @@
+import Navbar from "./NavBar";
+import heroImg from "../../assets/Hero/hero-img.png"; 
+import arrow from "../../assets/icons/line-end-circle-toRight.png"
+import timer from "../../assets/Hero/countDown-img.png"
 const Hero = () => {
-  return <section>{/* Let's go */}</section>;
+  return (<section id="home" className="">
+
+  
+  <div>
+
+<Navbar/>
+  </div>
+
+  <div className="container">
+
+<div className="flex justify-end flex py-60 space-x-20"> 
+  <div>
+    <span className="fontFamily-sans-serif text-6xl">Dare To Be </span>
+    <span className="text-transparent text-8xl bg-clip-text bg-gradient-to-r from-iwd-blue by-iwd-green to-iwd-green">
+	 Creative!
+</span>
+    </div> 
+  <div>
+    <img src={heroImg} alt="hero image" className=""></img>
+  </div>
+  
+  </div> 
+
+</div>
+<div>
+
+
+</div>
+  <div className="container">
+
+    <div className="flex flex-wrap justify-evenly py-20 flex-wrap max-width">
+      <div className="flex">
+        <img src={timer} alt="timer" width={"100%"} />
+  <img src={timer} alt="timer" width={"100%"} />
+  </div>
+  <div className="flex"><img src={timer} alt="timer" width={"100%"} />
+  <img src={timer} alt="timer" width={"100%"} /></div>
+  
+</div>
+ 
+
+  </div>
+<>
+
+</>
+  </section>
+  );
+
 };
 
 export default Hero;

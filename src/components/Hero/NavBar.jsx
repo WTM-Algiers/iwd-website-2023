7f467cd6297c8c6dd59cec617ca472c85d6bcf92
Hind26 <ledra.hind@gmail.com>
2023-02-23 19:12:15 +0100

    fixed timer with bg-image

--- src/components/Hero/NavBar.jsx
+++ src/components/Hero/NavBar.jsx
@@ -1,74 +1,73 @@
 import { useState } from "react";
 
-import wtmLogo from "../../assets/logos/wtm-algiers.png"; 
-function Navbar(){ //set links here 
-    let navLinks =[
-        {title:"Home",id:"/"},
-        {title:"About",id:"/"},
-        {title:"Previous",id:"/"},
-        {title:"Agenda",id:"/"},
-        {title:"Mentors & Judges",id:"/"},
-        {title:"Workshops",id:"/"},
-        {title:"Sponsors",id:"/"},
-      ];
-      
+import wtmLogo from "../../assets/logos/wtm-algiers.png";
+function Navbar() {
+  //set links here
+  let navLinks = [
+    { title: "Home", id: "/" },
+    { title: "About", id: "/" },
+    { title: "Previous", id: "/" },
+    { title: "Agenda", id: "/" },
+    { title: "Mentors & Judges", id: "/" },
+    { title: "Workshops", id: "/" },
+    { title: "Sponsors", id: "/" },
+  ];
 
-      const [active, setActive] = useState("Home");
-      const [toggle, setToggle] = useState(false);
-    
-      return (
-        <div className="bg-iwd-white top-0 w-screen z-20 fixed">
-  
-        <div className="container flex py-[38px] md:py-[2vw] justify-between align-center items-center">
-         <img src={wtmLogo} alt="WTM Logo" className="w-[9rem]" />
-   
-   <ul className="list-none sm:flex hidden justify-center gap-x-5 items-center">
-     {navLinks.map((nav, index) => (
-       <li
-         key={nav.id}
-         className={`cursor-pointer text-[16px] ${
-           active === nav.title ? "text-white" : "text-dimWhite"
-         }`}
-         onClick={() => setActive(nav.title)}
-       >
-         <a href={`#${nav.id}`}>{nav.title}</a>
-         
-       </li>
-     ))}
-     <button className="bg-iwd-dark px-4 py-1 text-iwd-white rounded-full">Register</button>
-   </ul>
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
 
-   <div className="sm:hidden flex flex-1 justify-end items-center">
-    
-<div className="flex flex-col gap-2">
-<div className="h-[2px] w-9 bg-iwd-black"></div>  <div className="h-[2px] w-9 bg-iwd-black"></div>  
-<div className="h-[2px] w-9 bg-iwd-black"></div>
-   </div>
-     
-     <div
-       className={`${
-         !toggle ? "hidden" : "flex"
-       } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-     >
+  return (
+    <div className="bg-iwd-white top-0 w-screen z-20 fixed">
+      <div className="container flex py-[38px] md:py-[2vw] justify-between align-center items-center">
+        <img src={wtmLogo} alt="WTM Logo" className="w-[9rem]" />
 
+        <ul className="list-none sm:flex hidden justify-center gap-x-5 items-center">
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className={`cursor-pointer text-[16px] ${
+                active === nav.title ? "text-white" : "text-dimWhite"
+              }`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+          <button className="bg-iwd-dark px-4 py-1 text-iwd-white rounded-full">
+            Register
+          </button>
+        </ul>
 
-       <ul className="list-none flex justify-end items-start flex-1 flex-col">
-         {navLinks.map((nav, index) => (
-           <li
-             key={nav.id}
-             className={`cursor-pointer text-[16px] ${
-               active === nav.title ? "text-white" : "text-dimWhite"
-             } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-             onClick={() => setActive(nav.title)}
-           >
-             <a href={`#${nav.id}`}>{nav.title}</a>
-           </li>
-         ))}
-       </ul>
-       </div>
-     </div>
-   </div>
-   </div>
-     )};
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <div className="flex flex-col gap-2">
+            <div className="h-[2px] w-9 bg-iwd-black"></div>{" "}
+            <div className="h-[2px] w-9 bg-iwd-black"></div>
+            <div className="h-[2px] w-9 bg-iwd-black"></div>
+          </div>
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          >
+            <ul className="list-none flex justify-end items-start flex-1 flex-col">
+              {navLinks.map((nav, index) => (
+                <li
+                  key={nav.id}
+                  className={`cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-dimWhite"
+                  } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                  onClick={() => setActive(nav.title)}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Navbar;
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,86 +1,70 @@
 import Navbar from "./NavBar";
-import heroImg from "../../assets/Hero/hero-img.png"; 
-import arrow from "../../assets/icons/line-end-circle-toRight.png"
-import timer from "../../assets/Hero/countDown-img.png"
-import arrowRight from "../../assets/icons/line-end-circle-toLeft.png"
+import heroImg from "../../assets/Hero/hero-img.png";
+import arrow from "../../assets/icons/line-end-circle-toRight.png";
+import timer from "../../assets/Hero/countDown-img.png";
+import arrowRight from "../../assets/icons/line-end-circle-toLeft.png";
 const Hero = () => {
-  return (<section id="home" className="">
-
-<div align="center" className="overflow-hidden">
-
-<div className="hidden md:flex md:flex-row gap-2 justify-start items-center md:mt-[9vw] md:-ml-36">
-
-<img src={arrow} alt="point-right" className=" w-[20%] h-[15%]"/>
-<span>Welcome to this beautiful community!</span>
-  </div>
-<div className="container">
-
-<div className="flex md:flex-row flex-col items-center gap-36 mt-[43.48837209302325vw] md:mt-0"> 
-  <div className="flex  flex-col justify-start items-start ">
-    <p className="md:text-left md:leading-[8.126934984520123vw] font-extrabold md:text-[6.5015479876160995vw] text-3xl">Dare to <br/> be <span
-class="md:text-left text-transparent bg-clip-text bg-gradient-to-r from-iwd-blue to-iwd-pink">
-	 Creative!
-</span></p>
-    
-<p className="text-left py-5">Do you “Dare To Be” this International Women’s Day by WTM Algiers chapter? </p>
-<button className="bg-iwd-dark px-6 py-2 text-iwd-white rounded-full">Register</button>
-    </div > 
-
-    <img src={heroImg} alt="hero image" className="md:w-[40%] md:h-[40%]"></img>
-   
-  </div>
- 
-
-</div>
-
-
-<div className="hidden  md:flex md:flex-row gap-2 justify-end items-center md:py-10 md:-mr-36">
-<span>Together we thrive!</span>
-<img src={arrowRight} alt="point-right" className=" w-[20vw]"/>
-
-  </div>
-  <div className="container">
-
-    <div className="grid grid-cols-2 gap-2 p-[2%] place-items-center md:flex md:flex-row md:justify-center md:items-center md:w-full md:px-20 mb-[12%] md:mb-16 md:space-x-20 w-full">
-      <div className="relative  w-[50%] my-3 md:w-[25%] md:h-[25%]">
-        <img src={timer} alt="timer" className="" />
-        <div className="absolute z-10 flex flex-col justify-center items-center bottom-2 left-6 md:bottom-8 md:left-11">
-        <div className="">00</div>
-        <div className="">Days</div>
+  return (
+    <section id="home" className="">
+      <div align="center" className="overflow-hidden">
+        <div className="hidden md:flex md:flex-row gap-2 justify-start items-center md:mt-[9vw] md:-ml-36">
+          <img src={arrow} alt="point-right" className=" w-[20%] h-[15%]" />
+          <span>Welcome to this beautiful community!</span>
         </div>
-        </div>
-        <div className="relative w-[50%] md:w-[25%] md:h-[25%]">
-        <img src={timer} alt="timer" className="" />
-        <div className="absolute z-10 flex flex-col justify-center items-center bottom-2 left-6 md:bottom-8 md:left-11">
-        <div className="">00</div>
-        <div className="">Hours</div>
-        </div>
+        <div className="container">
+          <div className="flex md:flex-row flex-col items-center md:gap-36 gap-12 mt-[33vw] md:mt-0">
+            <div className="flex flex-col justify-center items-center ">
+              <p className="md:text-left md:leading-[8.126934984520123vw] font-extrabold md:text-[6.5015479876160995vw] text-5xl">
+                Dare to <br /> be{" "}
+                <span class="text-transparent bg-clip-text bg-gradient-to-r from-iwd-blue to-iwd-pink">
+                  Creative!
+                </span>
+              </p>
+
+              <p className="md:text-left py-5 w-[80%]">
+                Do you “Dare To Be” this International Women’s Day by WTM
+                Algiers chapter?{" "}
+              </p>
+              <button className="bg-iwd-dark px-6 py-2 text-iwd-white rounded-full">
+                Register
+              </button>
+            </div>
+
+            <img
+              src={heroImg}
+              alt="hero image"
+              className="md:w-[40%] md:h-[40%] w-[80%] h-[80%]"
+            ></img>
+          </div>
         </div>
 
-
-        <div className="relative w-[50%] md:w-[25%] md:h-[25%]">
-  <img src={timer} alt="timer"/>
-  <div className="absolute z-10 flex flex-col justify-center items-center bottom-2 left-4 md:bottom-8 md:left-9">
-        <div className="">00</div>
-        <div className="">Minutes</div>
+        <div className="hidden  md:flex md:flex-row gap-2 justify-end items-center md:py-10 md:-mr-36">
+          <span>Together we thrive!</span>
+          <img src={arrowRight} alt="point-right" className=" w-[20vw]" />
         </div>
-  </div>
-  <div className="relative w-[50%] md:w-[25%] md:h-[25%]">
-  <img src={timer} alt="timer"/>
-  <div className="absolute z-10 flex flex-col justify-center items-center bottom-2 left-4 md:bottom-8 md:left-9">
-        <div className="">00</div>
-        <div className="">Seconds</div>
+        <div className="container">
+          <div className="grid grid-cols-2 gap-2 p-[2%] place-items-center md:flex md:flex-row md:justify-center md:items-center md:w-full md:px-20 mb-[12%] md:mb-16 md:space-x-20 w-full">
+            <div className="bg-[url('./assets/Hero/countDown-img.png')] w-[12vw] aspect-[1.29] bg-cover flex flex-col justify-center items-center ">
+              <div className="">00</div>
+              <div className="">Days</div>
+            </div>
+            <div className="bg-[url('./assets/Hero/countDown-img.png')] w-[12vw] aspect-[1.29] bg-cover flex flex-col justify-center items-center ">
+              <div className="">00</div>
+              <div className="">Hours</div>
+            </div>
+            <div className="bg-[url('./assets/Hero/countDown-img.png')] w-[12vw] aspect-[1.29] bg-cover flex flex-col justify-center items-center ">
+              <div className="">00</div>
+              <div className="">Minutes</div>
+            </div>
+            <div className="bg-[url('./assets/Hero/countDown-img.png')] w-[12vw] aspect-[1.29] bg-cover flex flex-col justify-center items-center ">
+              <div className="">00</div>
+              <div className="">Seconds</div>
+            </div>
+          </div>
         </div>
-  </div>
-</div>
- 
-
-  </div>
-
-</div>
-  </section>
+      </div>
+    </section>
   );
-
 };
 
 export default Hero;
